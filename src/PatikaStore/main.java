4131e68a39a06456622077337d5e0a962b55b1d8
volkankgil <volkankgil@gmail.com>
2022-11-11 17:20:11 +0300

    final case

--- src/PatikaStore/main.java
+++ src/PatikaStore/main.java
@@ -1,4 +1,14 @@
 package PatikaStore;
 
+
+import java.util.HashSet;
+import java.util.Scanner;
+import java.util.TreeSet;
+
 public class main {
+    public static void main(String[] args) {
+
+        store patikamaðaza = new store();
+        patikamaðaza.run();
+    }
 }
